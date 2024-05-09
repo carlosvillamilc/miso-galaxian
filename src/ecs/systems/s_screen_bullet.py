a6f9c60ce8b062b13392bf23551433b9f8e1e938
Carlos Villamil <c.villamilc@uniandes.edu.co>
2024-05-09 08:18:52 -0500

    se agrega sistema para bullets y servicio global

---
+++ src/ecs/systems/s_screen_bullet.py
@@ -0,0 +1,15 @@
+import pygame
+import esper
+
+from src.ecs.components.c_surface import CSurface
+from src.ecs.components.c_transform import CTransform
+from src.ecs.components.tags.c_tag_bullet import CTagBullet
+
+
+def system_screen_bullet(ecs_world: esper.World, screen: pygame.Surface):
+    screen_rect = screen.get_rect()
+    components = ecs_world.get_components(CTransform, CSurface, CTagBullet)
+    for bullet_entity, (c_t, c_s, _) in components:
+        bullet_rect = CSurface.get_area_relative(c_s.area, c_t.pos)
+        if not screen_rect.contains(bullet_rect):
+            ecs_world.delete_entity(bullet_entity)
---
+++ src/engine/services/globals_service.py
@@ -0,0 +1,6 @@
+class GlobalsService:
+    def __init__(self) -> None:
+        self.player_score = 0
+        self.player_high_score = 0
+        self.current_level = 0
+        self.paused = False
