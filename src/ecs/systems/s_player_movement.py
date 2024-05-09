047d8dd9ddc34417c7ed6b7876ed406e3dd452fb
Carlos Villamil <c.villamilc@uniandes.edu.co>
2024-05-08 22:31:17 -0500

    Ajuste movimiento jugador

--- src/ecs/systems/s_player_movement.py
+++ src/ecs/systems/s_player_movement.py
@@ -12,5 +12,5 @@ def system_player_movement(world: esper.World, screen: pygame.Surface, paused: b
     screen_rect = screen.get_rect()
     components = world.get_components(CTransform, CSurface, CTagPlayer)
     for _, (c_t, c_s, _) in components:
-        c_t.pos.x = max(screen_rect.left, min(c_t.pos.x, screen_rect.right - c_s.area.width))
+        c_t.pos.x = max(screen_rect.left + 20, min(c_t.pos.x, screen_rect.right - 20 - c_s.area.width))
         
