9d200dbd7101786b8f802e3e7536bda31b6d206e
Carlos Villamil <c.villamilc@uniandes.edu.co>
2024-05-15 21:30:50 -0500

    cambio nombre clase

--- src/ecs/components/c_game_manager.py
+++ src/ecs/components/c_game_manager.py
@@ -1,7 +1,7 @@
 from enum import Enum
 from src.engine.service_locator import ServiceLocator
 
-class PlayLevelState(Enum):
+class GameState(Enum):
     START = 0
     PLAY = 1
 
@@ -10,7 +10,7 @@ class CGameManager:
     def __init__(self, start_game_entity) -> None:
         interface_data = ServiceLocator.configs_service.get("assets/cfg/interface.json")
 
-        self.state:PlayLevelState = PlayLevelState.START
+        self.state:GameState = GameState.START
         self.current_game_time = 0
         self.start_game_time = interface_data['game_start_text']['time']
         self.start_game_entity = start_game_entity
